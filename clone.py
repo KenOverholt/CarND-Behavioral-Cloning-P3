a5e2d15dfecd6ed5e4625b626972fecb7b0d6c96
Ken Overholt <kroverholt@gmail.com>
2017-04-29 15:40:11 -0400

    Finish initial creation of clone.py

--- clone.py
+++ clone.py
@@ -23,3 +23,15 @@ for line in lines:
   
 X_train = np.array(images)
 y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add( Flatten(imput_shape=(160,320,3)) )
+model.add(Dense(1)
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True) #nb_epoch = 7
+
+model.save('model.h5)
