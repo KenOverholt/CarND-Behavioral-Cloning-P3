009aa00b6ef12191cc361cef238319a256fb244a
Ken Overholt <kroverholt@gmail.com>
2017-04-30 06:32:55 -0400

    Update clone.py with regularization

--- bin/clone.py
+++ bin/clone.py
@@ -28,8 +28,9 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-model.add( Flatten(input_shape=(160,320,3)) )
-model.add(Dense(1))
+model.add( Lambda( lambda x: (x/255.0) - 0.5, input_shape(160,320,3) ) )
+model.add( Flatten( input_shape=(160,320,3) ) )
+model.add( Dense(1) )
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True) #nb_epoch = 7
