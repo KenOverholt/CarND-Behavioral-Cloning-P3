16316a95538972eb535e957ba2ecba2686b85f64
KenOverholt <kroverholt@gmail.com>
2017-05-01 21:25:02 +0000

    nvidia architecture

--- bin/clone.py
+++ bin/clone.py
@@ -20,26 +20,35 @@ for line in lines:
   images.append(image)
   measurement = float(line[3])
   measurements.append(measurement)
+
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images, measurements):
+  augmented_images.append(image)
+  augmented_measurements.append(measurement)
+  augmented_images.append(cv2.flip(image,1))
+  augmented_measurements.append(measurement*-1.0)
   
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add( Lambda( lambda x: (x/255.0) - 0.5, input_shape=(160,320,3) ) )
-model.add( Flatten( input_shape=(160,320,3) ) )
+model.add( Lambda( lambda x: x/255.0 - 0.5, input_shape=(160,320,3) ) )
+model.add( Cropping2D( cropping=((70,25),(0,0)) ) )
 
-model.add( Convolution2D(6,5,5,activation="relu") )
-model.add( MaxPooling2D() )
-model.add( Convolution2D(2,5,5,activation="relu") )
-model.add( MaxPooling2D() )
+model.add( Convolution2D(24,5,5,subsample=(2,2),activation="relu") )
+model.add( Convolution2D(36,5,5,subsample=(2,2),activation="relu") )
+model.add( Convolution2D(48,5,5,subsample=(2,2),activation="relu") )
+model.add( Convolution2D(64,3,3,activation="relu") )
+model.add( Convolution2D(64,3,3,activation="relu") )
 model.add( Flatten() )
-model.add( Dense(120) )
-model.add( Dense(84) )
+model.add( Dense(100) )
+model.add( Dense(50) )
+model.add( Dense(10) )
 model.add( Dense(1) )
 
 model.compile(loss='mse', optimizer='adam')
