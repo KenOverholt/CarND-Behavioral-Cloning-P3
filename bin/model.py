d4b893ca123a07162504f233fedf390e6fdd3a6e
KenOverholt <kroverholt@gmail.com>
2017-05-04 10:18:06 +0000

    with dropout layer

--- bin/model.py
+++ bin/model.py
@@ -35,7 +35,7 @@ y_train = np.array(augmented_measurements)
 #print("X_train.shape: ", X_train.shape) #debug statement needed when the incorrect path was used above
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -53,6 +53,7 @@ model.add( Convolution2D(36,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(48,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(64,3,3,activation="relu") )
 model.add( Convolution2D(64,3,3,activation="relu") )
+model.add( Dropout( 0.1) )
 model.add( Flatten() )
 model.add( Dense(100) )
 model.add( Dense(50) )
@@ -60,6 +61,6 @@ model.add( Dense(10) )
 model.add( Dense(1) )
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
 
 model.save('model.h5')
