97ffe6d716f4a0643126eae328a11ecc9cfc5f2f
KenOverholt <kroverholt@gmail.com>
2017-05-04 00:33:15 +0000

    final version

--- bin/model.py
+++ bin/model.py
@@ -5,7 +5,7 @@ import numpy as np
 # Import driving data (images & speed).  Store in numpy arrays (Keras needs this format).
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('../data-fastest/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
     lines.append(line)
@@ -15,23 +15,44 @@ measurements = []
 for line in lines:
   source_path = line[0]
   filename = source_path.split('\\')[-1]
-  current_path = '../data/IMG/' + filename
+  current_path = '../data-fastest/IMG/' + filename
   image = cv2.imread(current_path)
   images.append(image)
   measurement = float(line[3])
   measurements.append(measurement)
-  
-X_train = np.array(images)
-y_train = np.array(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images, measurements):
+  augmented_images.append(image)
+  augmented_measurements.append(measurement)
+  augmented_images.append(cv2.flip(image,1))
+  augmented_measurements.append(measurement*-1.0)
+  
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+print("X_train.shape: ", X_train.shape)
+
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add( Flatten(input_shape=(160,320,3)) )
-model.add(Dense(1))
+model.add( Lambda( lambda x: x/255.0 - 0.5, input_shape=(160,320,3) ) )
+model.add( Cropping2D( cropping=((70,25),(0,0)) ) )
+
+model.add( Convolution2D(24,5,5,subsample=(2,2),activation="relu") )
+model.add( Convolution2D(36,5,5,subsample=(2,2),activation="relu") )
+model.add( Convolution2D(48,5,5,subsample=(2,2),activation="relu") )
+model.add( Convolution2D(64,3,3,activation="relu") )
+model.add( Convolution2D(64,3,3,activation="relu") )
+model.add( Flatten() )
+model.add( Dense(100) )
+model.add( Dense(50) )
+model.add( Dense(10) )
+model.add( Dense(1) )
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True) #nb_epoch = 7
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
