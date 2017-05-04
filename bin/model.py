9dd15e316db709e003ec3c7244c77faccb4b6d99
KenOverholt <kroverholt@gmail.com>
2017-05-04 22:33:36 +0000

    0008

--- bin/model.py
+++ bin/model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Import driving data (images & speed).  Store in numpy arrays (Keras needs this format).
 lines = []
-with open('../data-fastest/driving_log.csv') as csvfile:
+with open('../data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
     lines.append(line)
@@ -17,7 +17,7 @@ for line in lines:
   source_path = line[0]
   #store the name of the center camera's image
   filename = source_path.split('\\')[-1]
-  current_path = '../data-fastest/IMG/' + filename
+  current_path = '../data/IMG/' + filename
   image = cv2.imread(current_path) #retrieve the center camera's image
   images.append(image)
   measurement = float(line[3]) #retrieve the steering adjustment
@@ -53,7 +53,7 @@ model.add( Convolution2D(36,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(48,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(64,3,3,activation="relu") )
 model.add( Convolution2D(64,3,3,activation="relu") )
-model.add( Dropout( 0.05) )
+model.add( Dropout( 0.1) )
 model.add( Flatten() )
 model.add( Dense(100) )
 model.add( Dense(50) )
