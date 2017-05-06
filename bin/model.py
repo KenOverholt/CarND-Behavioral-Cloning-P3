948d9b21fe7b4c7682ab5714e1dfc0a8a4f2ab3b
KenOverholt <kroverholt@gmail.com>
2017-05-06 03:55:53 +0000

    working on 0013

--- bin/model.py
+++ bin/model.py
@@ -11,7 +11,7 @@ with open('../data-fastest/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
     lines.append(line)
-with open('../dirt-curbs/driving_log.csv') as csvfile:
+with open('../data-red-white-track/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
     lines.append(line)
@@ -22,15 +22,33 @@ with open('../data/driving_log.csv') as csvfile:
 print("line count: ", len(lines))
 
 # read in training data as is
+steering_correction = 0.2
 for line in lines:
-  source_path = line[0]
-  #store the name of the center camera's image
-  filename = source_path.split('\\')[-1]
-  current_path = '../data/IMG/' + filename
-  image = cv2.imread(current_path) #retrieve the center camera's image
-  images.append(image)
-  measurement = float(line[3]) #retrieve the steering adjustment
-  measurements.append(measurement)
+  center_source_path = line[0]
+  left_source_path = line[1]
+  right_source_path = line[2]
+ #store the name of the center camera's image
+  current_path = '../data/IMG/'  
+  center_filename = center_source_path.split('\\')[-1]
+  left_filename   = left_source_path.split('\\')[-1]
+  right_filename  = right_source_path.split('\\')[-1]
+  center_fullfilename = current_path + center_filename
+  left_fullfilename   = current_path + left_filename
+  right_fullfilename  = current_path + right_filename
+  center_image = cv2.imread(center_fullfilename) #retrieve the center camera's image
+  left_image   = cv2.imread(left_fullfilename)
+  right_image  = cv2.imread(right_fullfilename)
+
+  images.append(center_image)
+  images.append(left_image)
+  images.append(right_image)  
+
+  center_measurement = float(line[3]) #retrieve the steering adjustment
+  left_measurement = center_measurement + steering_correction
+  right_measurement = center_measurement - steering_correction  
+  measurements.append(center_measurement)
+  measurements.append(left_measurement)
+  measurements.append(right_measurement) 
 
 augmented_images, augmented_measurements = [], []
 for image,measurement in zip(images, measurements):
@@ -58,6 +76,7 @@ model.add( Cropping2D( cropping=((70,25),(0,0)) ) )
 
 # create the nvidia network
 model.add( Convolution2D(24,5,5,subsample=(2,2),activation="relu") )
+#model.add( Dropout(0.05) )
 model.add( Convolution2D(36,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(48,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(64,3,3,activation="relu") )
@@ -65,8 +84,8 @@ model.add( Convolution2D(64,3,3,activation="relu") )
 model.add( Flatten() )
 model.add( Dense(100) )
 model.add( Dense(50) )
-model.add( Dropout(0.1) )
 model.add( Dense(10) )
+model.add( Dropout(0.05) )
 model.add( Dense(1) )
 
 model.compile(loss='mse', optimizer='adam')
