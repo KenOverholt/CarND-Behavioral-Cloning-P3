27901c41be934eed6427dd23f65b6faf836e9ccf
KenOverholt <kroverholt@gmail.com>
2017-05-06 18:15:54 +0000

    0013

--- bin/model.py
+++ bin/model.py
@@ -1,65 +1,74 @@
+import os
 import csv
-import cv2
-import numpy as np
 
 # Import driving data (images & speed).  Store in numpy arrays (Keras needs this format).
-lines = []
-images = []
-measurements = []
+samples = []
 
 with open('../data-fastest/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
-    lines.append(line)
+    samples.append(line)
 with open('../data-red-white-track/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
-    lines.append(line)
+    samples.append(line)
 with open('../data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile)
   for line in reader:
-    lines.append(line)
-print("line count: ", len(lines))
+    samples.append(line)
+print("line count: ", len(samples))
 
-# read in training data as is
-steering_correction = 0.2
-for line in lines:
-  center_source_path = line[0]
-  left_source_path = line[1]
-  right_source_path = line[2]
- #store the name of the center camera's image
-  current_path = '../data/IMG/'  
-  center_filename = center_source_path.split('\\')[-1]
-  left_filename   = left_source_path.split('\\')[-1]
-  right_filename  = right_source_path.split('\\')[-1]
-  center_fullfilename = current_path + center_filename
-  left_fullfilename   = current_path + left_filename
-  right_fullfilename  = current_path + right_filename
-  center_image = cv2.imread(center_fullfilename) #retrieve the center camera's image
-  left_image   = cv2.imread(left_fullfilename)
-  right_image  = cv2.imread(right_fullfilename)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size = 0.2)
+print("train_samples count: ", len(train_samples))
+print("validation_samples count : ", len(validation_samples))
 
-  images.append(center_image)
-  images.append(left_image)
-  images.append(right_image)  
+import cv2
+import numpy as np
+import sklearn
+import random
 
-  center_measurement = float(line[3]) #retrieve the steering adjustment
-  left_measurement = center_measurement + steering_correction
-  right_measurement = center_measurement - steering_correction  
-  measurements.append(center_measurement)
-  measurements.append(left_measurement)
-  measurements.append(right_measurement) 
+def generator(samples, batch_size=32):
+  num_samples = len(samples)
+  while 1:
+    random.shuffle(samples)
+    for offset in range(0, num_samples, batch_size):
+      batch_samples = samples[offset:offset+batch_size]
+      images = []
+      angles = []
+      steering_correction = 0.2
+      for batch_sample in batch_samples:
+        current_path = '../data/IMG/'  
+        center_image = cv2.imread(current_path + batch_sample[0].split('\\')[-1])
+        left_image   = cv2.imread(current_path + batch_sample[1].split('\\')[-1])
+        right_image  = cv2.imread(current_path + batch_sample[2].split('\\')[-1])
+        images.append(center_image)
+        images.append(left_image)
+        images.append(right_image)  
 
-augmented_images, augmented_measurements = [], []
-for image,measurement in zip(images, measurements):
-  augmented_images.append(image) #store the normal image for training
-  augmented_measurements.append(measurement) #store the normal measurement for training
-  augmented_images.append(cv2.flip(image,1)) #store a vertically-flipped image for training
-  augmented_measurements.append(measurement*-1.0) #store the reversed steering measurement for training
-  
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
-#print("X_train.shape: ", X_train.shape) #debug statement needed when the incorrect path was used above
+        center_angle = float(batch_sample[3]) #retrieve the steering adjustment
+        left_angle = center_angle + steering_correction
+        right_angle = center_angle - steering_correction  
+        angles.append(center_angle)
+        angles.append(left_angle)
+        angles.append(right_angle) 
+
+        augmented_images, augmented_angles = [], []
+        for image,angle in zip(images, angles):
+          augmented_images.append(image) #store the normal image for training
+          augmented_angles.append(angle) #store the normal angle for training
+          augmented_images.append(cv2.flip(image,1)) #store a vertically-flipped image for training
+          augmented_angles.append(angle*-1.0) #store the reversed steering angle for training
+      
+        X_train = np.array(augmented_images)
+        y_train = np.array(augmented_angles)
+        #print("X_train.shape: ", X_train.shape)
+      yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size = 32)
+validation_generator = generator(validation_samples, batch_size = 32)
+
 
 from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
@@ -74,9 +83,8 @@ model.add( Lambda( lambda x: x/255.0 - 0.5, input_shape=(160,320,3) ) )
 #  the network to focus on the relevant parts of the image.
 model.add( Cropping2D( cropping=((70,25),(0,0)) ) )
 
-# create the nvidia network
+# create the nvidia network with slight modification(s)
 model.add( Convolution2D(24,5,5,subsample=(2,2),activation="relu") )
-#model.add( Dropout(0.05) )
 model.add( Convolution2D(36,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(48,5,5,subsample=(2,2),activation="relu") )
 model.add( Convolution2D(64,3,3,activation="relu") )
@@ -85,10 +93,13 @@ model.add( Flatten() )
 model.add( Dense(100) )
 model.add( Dense(50) )
 model.add( Dense(10) )
-model.add( Dropout(0.05) )
+model.add( Dropout(0.05) ) # added to help reduce overfitting
 model.add( Dense(1) )
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples*3*2),
+                    validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples), nb_epoch=5)
 
 model.save('model.h5')
